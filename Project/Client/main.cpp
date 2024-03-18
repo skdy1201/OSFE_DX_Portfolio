4bdda09769a7da11aceb4607d8312bcd77022b6d
SEO DONG YUN <skdy1201@gmail.com>
2024-03-18 16:34:53 +0900

    CodeGen 개선 (Script 프로젝트에 Script 필터 내에 있는것만 갱신하도록)

--- Project/Client/main.cpp
+++ Project/Client/main.cpp
@@ -52,7 +52,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_ int       nCmdShow)
 {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-    //_CrtSetBreakAlloc(433);
+    //_CrtSetBreakAlloc(1428043);
 
     MyRegisterClass(hInstance);
 
